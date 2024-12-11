bdbb1fac54854e4432b121778bf53f336c083ed7
Zach Buchli <zachbuchli@pm.me>
2024-12-10 18:49:15 -0800

    was using the wrong args

--- plugin/weather.lua
+++ plugin/weather.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_user_command('Weather', function(opts)
   local location
-  if string.len(opts.args) > 0 then
-    location = opts.args
+  if string.len(opts.fargs) > 0 then
+    location = opts.fargs[1]
   end
   require('weather').show(location)
 end, {
